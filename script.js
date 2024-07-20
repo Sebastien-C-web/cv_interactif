ea3604f577a60e18f49bb160de8369415212c4e6
sebastien chadozeau <sebastien.chadozeau@gmail.com>
2024-07-20 16:50:50 +0200

    upd

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ let btndip = document.getElementById("diplome");
 let btnxp = document.getElementById("xp");
 let btnlang = document.getElementById("langue");
 let comp = document.getElementById('comp');
+let dip = document.getElementById('dip');
+let main = document.getElementById('main')
 
-console.log(comp)
-
+console.log(main)
 btncomp.addEventListener('click', ()=> {
     if (comp.classList.contains('hidden')) {
         comp.classList.remove('hidden');
+        comp.classList.add('max-lg:flex');
     } else {
         comp.classList.add('hidden');
+        comp.classList.remove('max-lg:flex');
     }
 })
+btndip.addEventListener('click', ()=> {
+    if (dip.classList.contains('hidden')) {
+        dip.classList.remove('hidden');
+        dip.classList.add('max-lg:flex');
+        main.classList.add('h-[180vh]');
+        
+    } else {
+        dip.classList.add('hidden');
+        dip.classList.remove('max-lg:flex');
+        dip.classList.remove('h-[180vh]')
+        
+    }
+})
+
